ef4d4f82947b517a80d3737a0853b63a6d3ae400
rostamali <rostamsardar446@gmail.com>
2021-10-30 11:47:31 -0700

    set delete btn

--- index.js
+++ index.js
@@ -56,6 +56,14 @@ async function run() {
         const orders = await query.toArray();
         res.send(orders);
       })
+
+      // delete my orders
+      app.delete('/order/:id', async (req, res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await orderCollection.deleteOne(query);
+        res.send(result);
+      })
       
     } finally {
     //   await client.close();
